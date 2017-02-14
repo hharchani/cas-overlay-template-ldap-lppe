5aa0ae51ffa2721d0c7b3fc03ec3e4da8318db3c
Harshit Harchani <hharchani@gmail.com>
2017-02-14 20:07:16 +0530

    Add new theme
    Copied the old css file on cas.original.css

--- src/main/webapp/WEB-INF/view/jsp/default/ui/includes/top.jsp
+++ src/main/webapp/WEB-INF/view/jsp/default/ui/includes/top.jsp
@@ -9,25 +9,23 @@
 
 <html lang="en">
 <head>
-  <meta charset="UTF-8" />
+    <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>CAS &#8211; Central Authentication Service</title>
+    <title>Central Authentication Service - IIIT Hyderabad</title>
 
     <meta name="_csrf" content="${_csrf.token}"/>
     <meta name="_csrf_header" content="${_csrf.headerName}"/>
-
 
     <spring:theme code="standard.custom.css.file" var="customCssFile" />
     <link rel="stylesheet" href="<c:url value="${customCssFile}" />" />
     <link rel="icon" href="<c:url value="/favicon.ico" />" type="image/x-icon" />
 
 </head>
-<body id="cas">
-
-<div id="container">
-  <header>
-    <a id="logo" href="http://www.apereo.org" title="<spring:message code="logo.title" />">Apereo</a>
-    <h1>Central Authentication Service (CAS)</h1>
-  </header>
-  <div id="content">
+<body>
+    <div class="container">
+        <header>
+            <img class="logo" src="images/iiit-new.png" />
+        </header>
+        <main class="content">
+            <h1>Central Authentication Service (CAS)</h1>
--- src/main/webapp/js/cas.js
+++ src/main/webapp/js/cas.js
@@ -1,62 +1,113 @@
+(function() {
+    /**
+     * Utility for Cookies
+     * @namespace cookies
+     * @author Nisarg Jhaveri <nisarg.jhaveri@students.iiit.ac.in>
+     *
+     * Originally written for Felicity '15 website
+     */
+    var cookies = {
+        /**
+         * Add or update cookie
+         *
+         * @memberof cookies
+         *
+         * @param {string} name - Name of the cookie
+         * @param {string} value - Value to be set
+         * @param {number} expiry - Expiry of cookie in hours (i.e expire after this many hours)
+         * @param {string} [path] - Path for the cookie
+         */
+        set: function( name, value, expiry, path ) {
+            var ex = new Date();
+            ex.setTime( ex.getTime() + ( expiry * 60 * 60 * 1000 ) );
+            var expires = "expires=" + ex.toUTCString();
+            document.cookie = name + "=" + value + "; " + expires + ( path ? '; path=' + path : '' );
+        },
+        /**
+         * Get required cookie value
+         *
+         * @memberof cookies
+         *
+         * @param {string} name - Name of the cookie
+         *
+         * @returns {string} Value of the cookie
+         */
+        get: function( name ) {
+            name = name + '=';
+            var cookieList = document.cookie.split( ';' );
+            var cookie;
+            for ( cookie in cookieList ) {
+                cookie = cookieList[cookie].trim();
+                if ( cookie.substr( 0, name.length ) == name ) {
+                    return cookie.substr( name.length );
+                }
+            }
+        },
+        /**
+         * Remove a cookie
+         *
+         * @memberof cookies
+         *
+         * @param {string} name - Name of the cookie
+         */
+        remove: function ( name ) {
+            document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+        }
+    };
 
-head.ready(document, function() {
-    head.load("https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js", loadjQueryUI);
-});
-
-function loadjQueryUI() {
-    head.load("https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js", loadjQueryCookies);
-}
-
-function loadjQueryCookies() {
-    head.load("https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js", resourceLoadedSuccessfully);
-}
-
-function areCookiesEnabled() {
-    $.cookie('cookiesEnabled', 'true');
-    var value = $.cookie('cookiesEnabled');
-    if (value != undefined) {
-        $.removeCookie('cookiesEnabled');
+    var areCookiesDisabled = function () {
+        cookies.set('testCookie', 'true', 1);
+        var value = cookies.get('testCookie');
+        if (value !== undefined) {
+            cookies.remove('testCookie');
+            return false;
+        }
         return true;
-    }
-    return false;
-}
+    };
 
-function resourceLoadedSuccessfully() {
-    $(document).ready(function() {
+    var emailDomains = [
+        'iiit.ac.in',
+        'students.iiit.ac.in',
+        'research.iiit.ac.in',
+    ];
+    var onLoad = function() {
+        var emailBox = document.getElementById('username');
+        var passwordBox = document.getElementById('password');
+        var capslockOnMessage = document.getElementById('capslock-on');
 
-        if ($(":focus").length === 0){
-            $("input:visible:enabled:first").focus();
+        // Check if cookies is disabled
+        if (areCookiesDisabled()) {
+            document.getElementById('cookiesDisabled').style.display = 'block';
         }
 
+        // Focus email textbox
+        emailBox.focus();
 
-        if (areCookiesEnabled()) {
-            $('#cookiesDisabled').hide();
-        } else {
-            $('#cookiesDisabled').show();
-            $('#cookiesDisabled').animate({ backgroundColor: 'rgb(187,0,0)' }, 30).animate({ backgroundColor: 'rgb(255,238,221)' }, 500);
-        }
-
-        //flash error box
-        $('#msg.errors').animate({ backgroundColor: 'rgb(187,0,0)' }, 30).animate({ backgroundColor: 'rgb(255,238,221)' }, 500);
-
-        //flash success box
-        $('#msg.success').animate({ backgroundColor: 'rgb(51,204,0)' }, 30).animate({ backgroundColor: 'rgb(221,255,170)' }, 500);
-
-        //flash confirm box
-        $('#msg.question').animate({ backgroundColor: 'rgb(51,204,0)' }, 30).animate({ backgroundColor: 'rgb(221,255,170)' }, 500);
-
-        $('#capslock-on').hide();
-        $('#password').keypress(function(e) {
-            var s = String.fromCharCode( e.which );
-            if ( s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey ) {
-                $('#capslock-on').show();
-            } else {
-                $('#capslock-on').hide();
+        // Autocomplete email domain
+        emailBox.addEventListener('keyup', function (e) {
+            if ((e.which >= 'A'.charCodeAt(0) && e.which <= 'Z'.charCodeAt(0) )
+            || (e.which >= 'a'.charCodeAt(0) && e.which <= 'z'.charCodeAt(0) )) {
+                var reg = new RegExp('^' + emailBox.value.split('@')[1]);
+                var allowed = emailDomains.filter(function(domain) {
+                    return reg.test(domain);
+                });
+                if (allowed.length == 1) {
+                    emailBox.value = emailBox.value.split('@')[0] + '@' + allowed[0];
+                    passwordBox.focus();
+                }
             }
         });
-        if (typeof(jqueryReady) == "function") {
-            jqueryReady();
-        }
-    });
 
-};
+        // Show warning if capslock is on
+        passwordBox.addEventListener('keypress', function(e) {
+            var s = String.fromCharCode( e.which );
+            if ( s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey ) {
+                capslockOnMessage.style.display = 'inline';
+            } else {
+                capslockOnMessage.style.display = 'none';
+            }
+        });
+    };
+
+    window.addEventListener('load', onLoad);
+})();
